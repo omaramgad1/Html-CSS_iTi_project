8a2072198dda71287f535b179cc05de459616093
omaramgad1 <omaramgad89@gmail.com>
2023-01-16 04:31:28 +0200

    last_update

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,19 @@ const navbar = document.querySelector(".navbar");
 const menu = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = ()=>{
+menuBtn.onclick = () => {
   menu.classList.add("active");
   menuBtn.classList.add("hide");
   cancelBtn.classList.add("show");
   body.classList.add("disabledScroll");
 }
-cancelBtn.onclick = ()=>{
+cancelBtn.onclick = () => {
   menu.classList.remove("active");
   menuBtn.classList.remove("hide");
   cancelBtn.classList.remove("show");
   body.classList.remove("disabledScroll");
 }
-window.onscroll = ()=>{
+window.onscroll = () => {
   this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
- 
+
